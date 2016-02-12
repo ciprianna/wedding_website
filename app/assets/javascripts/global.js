d5ab9da56dcbda7552abd5d7d5288e7f1e567d63
Ciprianna <ciprianna17@gmail.com>
2016-02-11 22:32:26 -0600

    Added menu icon functionality!!!

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,3 +1,7 @@
+$(window).bind("load", function() {
+  navmenu = document.getElementById("menu_link");
+  navmenu.addEventListener("click", show_menu);
+});
 
 function show_menu() {
   nav = document.getElementById("home");
@@ -6,18 +10,20 @@ function show_menu() {
 	} else {
 		nav.className = "hide_home";
 	}
-}
-
-menu = document.getElementById("menu_link");
-menu.addEventListener("click", show_menu);
-menu.addEventListener("click", show_sub_nav);
+};
 
 
-function show_sub_nav() {
-  sub_nav = document.getElementById("sub_nav");
-  if(sub_nav.className == "hide_home"){
-    sub_nav.className = "show_home";
-  } else {
-    sub_nav.className = "hide_home";
-  }
-}
+
+
+
+
+
+//
+// function show_sub_nav() {
+//   sub_nav = document.getElementById("sub_nav");
+//   if(sub_nav.className == "hide_home"){
+//     sub_nav.className = "show_home";
+//   } else {
+//     sub_nav.className = "hide_home";
+//   }
+// }
