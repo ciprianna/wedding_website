abf3913647316d5af9ceafa1739b2bc64a41ae8e
Ciprianna <ciprianna17@gmail.com>
2016-02-15 22:14:55 -0600

    Hide menu icon when nav menu is out; close arrow closes window.

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,22 +1,35 @@
 $(window).bind("load", function() {
   navmenu = document.getElementById("menu_link");
   navmenu.addEventListener("click", show_menu);
+  closeNav = document.getElementsByClassName("close_menu");
+  closeNav[0].addEventListener("click", close_nav);
 });
 
 function show_menu() {
   nav = document.getElementById("home");
   cloak = document.getElementById("c-mask");
+  navicon = document.getElementsByClassName("menu_icon");
   if(nav.className == "hide_home"){
 		nav.className = "show_home";
     cloak.className = "c-mask is-active";
+    navicon[0].className = "menu_icon hide_element";
 	} else {
-		nav.className = "hide_home";
+    nav.className = "hide_home";
     cloak.className = "c-mask";
+    navicon[0].className = "menu_icon show_element";
 	}
 };
 
+function close_nav() {
+  nav = document.getElementById("home");
+  cloak = document.getElementById("c-mask");
+  navicon = document.getElementsByClassName("menu_icon");
+  nav.className = "hide_home";
+  cloak.className = "c-mask";
+  navicon[0].className = "menu_icon show_element";
+};
 
-// --Switch links and buttons on nav so entire button is clicked
+
 // --Activate left arrow to close menu; hide menu icon
 // --Remove menu when link is clicked
 // --Deactivate scrolling when menu is active
