8df5b5dddf8897b42abe797889bf0bf70e8ee189
Ciprianna <ciprianna17@gmail.com>
2016-02-17 20:08:55 -0600

    Removed scrolling from background when menu is open.

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -13,14 +13,17 @@ function show_menu() {
   nav = document.getElementById("home");
   cloak = document.getElementById("c-mask");
   navicon = document.getElementsByClassName("menu_icon");
+  body = document.getElementsByTagName("body");
   if(nav.className == "hide_home"){
 		nav.className = "show_home";
     cloak.className = "c-mask is-active";
     navicon[0].className = "menu_icon hide_element";
+    body[0].setAttribute("style", "overflow: hidden;");
 	} else {
     nav.className = "hide_home";
     cloak.className = "c-mask";
     navicon[0].className = "menu_icon show_element";
+    body[0].setAttribute("style", "overflow: auto;");
 	}
 };
 
@@ -28,11 +31,16 @@ function close_nav() {
   nav = document.getElementById("home");
   cloak = document.getElementById("c-mask");
   navicon = document.getElementsByClassName("menu_icon");
+  body = document.getElementsByTagName("body");
   nav.className = "hide_home";
   cloak.className = "c-mask";
   navicon[0].className = "menu_icon show_element";
+  body[0].setAttribute("style", "overflow: auto;");
 };
 
 
-// --Remove menu when link is clicked
-// --Deactivate scrolling when menu is active
+// --Pin header to top when scroll
+// --Fix RSVP button (style and functionality)
+// --Scroll background photos at different speeds
+// --Update text to match wedding details
+// --HOST!!!
