52d6d84ca56ccef6b4bcc433de9ea408679dec46
Ciprianna <ciprianna17@gmail.com>
2016-03-23 18:38:19 -0500

    Added an activehash gem

--- app/controllers/api/v1/guests_controller.rb
+++ app/controllers/api/v1/guests_controller.rb
@@ -1,7 +1,23 @@
 class Api::V1::GuestsController < Api::V1::BaseController
+  include ActiveHashRelation
+
   def show
     guest = Guests.find(params[:id])
 
     render(json: Api::V1::GuestsSerializer.new(guest).to_json)
   end
+
+  def index
+  guests = Guests.all
+
+  guests = apply_filters(guests, params)
+
+  render(
+    json: ActiveModel::ArraySerializer.new(
+      guests,
+      each_serializer: Api::V1::GuestsSerializer,
+      root: 'guests',
+    )
+  )
+end
 end
